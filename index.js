877c822cfc12dc33fa3ecae71eee5539b0445bb4
jackokramer <jackokramer@gmail.com>
2022-08-31 15:10:13 -0700

    replit transfer

---
+++ index.js
@@ -0,0 +1,15 @@
+// Obtain the img element, and assign it to the image variable
+let image
+
+// Obtain the 'shrink-grow' button, and assign it to the changeSize variable
+let changeSize
+
+//DO NOT CHANGE THE FOLLOWING CODE
+changeSize.addEventListener("click", function() {
+  if (image.style.height == "100px") {
+    image.style.height = "25px"
+  }
+  else {
+    image.style.height = "100px"
+  }
+})
