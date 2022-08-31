7ef51bf8611619108122d033c942f539ec595ba5
Veronica O'Brien <veronicaobrien@Veronicas-MBP.lan>
2022-08-31 19:52:56 -0400

    final

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Obtain the img element, and assign it to the image variable
 let image = document.getElementById('#pet');
-
+console.log(image)
 
 // Obtain the 'shrink-grow' button, and assign it to the changeSize variable
 let changeSize = document.getElementById('#shrink-grow')
-
+console.log(changeSize)
 
 //DO NOT CHANGE THE FOLLOWING CODE
 changeSize.addEventListener("click", function() {
